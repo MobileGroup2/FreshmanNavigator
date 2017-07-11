6545450d92f695c4c36e315109ae2a6758a580b9
alexquesenberry <ajq15b@gmail.com>
2017-07-11 17:48:25 -0400

    HomeScreen changed, all images added which may be used for future use

--- app/src/main/java/francefrancerevolution/gonolesdatabase/model/HomeScreen.java
+++ app/src/main/java/francefrancerevolution/gonolesdatabase/model/HomeScreen.java
@@ -1,5 +1,4 @@
 
-
 package francefrancerevolution.gonolesdatabase.model;
 
 
@@ -7,7 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.ImageButton;
 
 import francefrancerevolution.gonolesdatabase.R;
 
