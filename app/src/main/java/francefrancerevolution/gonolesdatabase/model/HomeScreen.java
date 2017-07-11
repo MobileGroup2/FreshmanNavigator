3a3c8ec339918233ff215fa4a53f5b8e7ce40781
sap15e <anjalibrown12@gmail.com>
2017-07-11 16:25:47 -0400

    I made a homescreen.java file for the buttons that say map,buildings,and schedule. I changed the android manifest file so that the app does not go to the list of buildings when clicked on and now shows the three buttons for the user to select from

---
+++ app/src/main/java/francefrancerevolution/gonolesdatabase/model/HomeScreen.java
@@ -0,0 +1,47 @@
+
+//ANJALI DID THIS
+package francefrancerevolution.gonolesdatabase.model;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import francefrancerevolution.gonolesdatabase.R;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+        Button maps = (Button) findViewById(R.id.map);
+        Button schedule = (Button) findViewById(R.id.schedule);
+        Button buildings = (Button) findViewById(R.id.buildings);
+
+
+
+
+
+    }
+
+
+    public void mapsClick(View v)
+    {
+
+    }
+
+    public void scheduleClick(View v)
+    {
+
+    }
+
+    public void buildingClick(View v)
+    {
+      ;
+    }
+
+}
