acdbb34727b73d2f35f3eb9af4e9ebb2c6685cf6
Alucard5 <0^hukGjDQ%jK>
2017-07-11 19:21:45 -0400

    Added Maps Activity

--- app/src/main/java/francefrancerevolution/gonolesdatabase/MapsActivity.java
+++ app/src/main/java/francefrancerevolution/gonolesdatabase/MapsActivity.java
@@ -1,7 +1,11 @@
 package francefrancerevolution.gonolesdatabase;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,8 +26,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+    }
 
     /**
      * Manipulates the map once available.
@@ -39,8 +55,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng fsu = new LatLng(30.4438320,-84.296339);
+        mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(fsu , 15.0f) );
+
+       // mMap.addMarker(new MarkerOptions().position(fsu).title("FSU"));
+       // mMap.moveCamera(CameraUpdateFactory.newLatLng(fsu));
     }
 }
