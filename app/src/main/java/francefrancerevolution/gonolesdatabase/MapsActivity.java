f45d3e876551c8322170940ad97f0daca95c6944
Alucard5 <0^hukGjDQ%jK>
2017-07-12 16:16:35 -0400

    Check if GPS is enbled. Create dialog to open location settings to enable GPS if not enabled.

--- app/src/main/java/francefrancerevolution/gonolesdatabase/MapsActivity.java
+++ app/src/main/java/francefrancerevolution/gonolesdatabase/MapsActivity.java
@@ -2,6 +2,7 @@ package francefrancerevolution.gonolesdatabase;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -12,6 +13,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -19,14 +22,16 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import francefrancerevolution.gonolesdatabase.model.HomeScreen;
 
 import static android.R.attr.enabled;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    static AlertDialog alert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +42,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        //Check if GPS is enabled
         LocationManager mlocManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         boolean enabled = mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
@@ -44,60 +50,75 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             showDialogGPS();
         }
 
+
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        //Check GPS Permissions
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
             return;
         }
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
         mMap.setMyLocationEnabled(true);
-
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         // Add a marker in Sydney and move the camera
-        LatLng fsu = new LatLng(30.4438320,-84.296339);
+        LatLng fsu = new LatLng(30.441668, -84.298517);
         mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(fsu , 15.0f) );
+
+
 
        // mMap.addMarker(new MarkerOptions().position(fsu).title("FSU"));
        // mMap.moveCamera(CameraUpdateFactory.newLatLng(fsu));
+
+
+        mMap.setOnMarkerClickListener(
+                new GoogleMap.OnMarkerClickListener() {
+                    @Override
+                    public boolean
+                    onMarkerClick(Marker arg0) {
+                        LatLng position = arg0.getPosition();
+                        Toast.makeText(getApplicationContext(),"Lat: " + position.latitude + ", Lng: "+ position.longitude,Toast.LENGTH_LONG).show();
+                                return true;
+                            }
+                        });
+        mMap.setOnMapClickListener(
+                new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void
+                    onMapClick(LatLng arg0) {
+                        Toast.makeText(getApplicationContext(),"Lat: "+ arg0.latitude + ", Lng: " + arg0.longitude,Toast.LENGTH_LONG).show();
+                            }
+                        });
     }
 
     private void showDialogGPS() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setCancelable(false);
-        builder.setTitle("Enable GPS");
-        builder.setMessage("Please enable GPS");
-        builder.setInverseBackgroundForced(true);
-        builder.setPositiveButton("Enable", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                startActivity(
-                        new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-            }
-        });
-        builder.setNegativeButton("Ignore", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog);
+
+        Button buttonOk = (Button) dialog.findViewById(R.id.dialogOkButton);
+        Button buttonCancel = (Button) dialog.findViewById(R.id.dialogCancelButton);
+
+        buttonOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+
+                getBaseContext().startActivity(intent);
                 dialog.dismiss();
             }
         });
-        AlertDialog alert = builder.create();
-        alert.show();
+
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        });
+        dialog.show();
     }
 }
