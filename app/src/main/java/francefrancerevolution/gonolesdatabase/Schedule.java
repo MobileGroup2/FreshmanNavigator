9a97458da2b6603e44df3842ae5115ea251aa2c2
alexquesenberry <ajq15b@gmail.com>
2017-07-12 17:34:02 -0400

    Adjust home screen, adjust schedule.xml and .java in order to be the MAIN view when clicking on class from home screen, then from this main view you can go to add_class.xml view which will allow you to add classes to you schedule

--- app/src/main/java/francefrancerevolution/gonolesdatabase/Schedule.java
+++ app/src/main/java/francefrancerevolution/gonolesdatabase/Schedule.java
@@ -2,37 +2,17 @@ package francefrancerevolution.gonolesdatabase;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 public class Schedule extends AppCompatActivity {
 
+    private Button addClasses;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.schedule);
+        addClasses = (Button)findViewById(R.id.button_add_classes);
 
-//        Button resetButton = (Button) findViewById(R.id.button_reset);
-//        Button registerButton = (Button) findViewById(R.id.button_register);
-//        Spinner myspinner = (Spinner)findViewById(R.id.spinner);
-//        String[] arraySpinner;
-//        arraySpinner = new String[] {
-//                "Biology", "Physics","Computer Science", "Engineering", "Chemistry", "Statistics"
-//        };
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-//                android.R.layout.simple_spinner_item, arraySpinner);
-//        myspinner.setAdapter(adapter);
-//
-//
-//
-//        resetButton.setOnClickListener(mResetListener);
-//        registerButton.setOnClickListener(mRegisterListener);
     }
 
 
