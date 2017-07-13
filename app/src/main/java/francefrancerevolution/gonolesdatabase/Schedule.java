44c1be1b2fb6cbebda9bc38eea200c35c373bdf6
sap15e <anjalibrown12@gmail.com>
2017-07-13 14:59:41 -0400

    Made AddClass.java and added functionality to ADD CLASSES button in Schedule.java and once user clicks on that, will be sent to the addclass.java which will let user add a class. anjali

--- app/src/main/java/francefrancerevolution/gonolesdatabase/Schedule.java
+++ app/src/main/java/francefrancerevolution/gonolesdatabase/Schedule.java
@@ -1,8 +1,13 @@
 package francefrancerevolution.gonolesdatabase;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+
+import francefrancerevolution.gonolesdatabase.model.AddClass;
+
 
 public class Schedule extends AppCompatActivity {
 
@@ -11,8 +16,15 @@ public class Schedule extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.schedule);
-        //addClasses = (Button)findViewById(R.id.button_add_classes);
+        addClasses = (Button)findViewById(R.id.button_add_classes);
 
+
+    }
+
+    public void classClick(View v)
+    {
+        Intent intent = new Intent(Schedule.this, AddClass.class);
+        startActivity(intent);
     }
 
 
